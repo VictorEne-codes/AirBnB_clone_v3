672b1067069bff66313ff595b6cb52920a7283f7
victor <omalevicene@gmail.com>
2025-02-17 14:44:46 -0800

    agaij

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,52 +1,33 @@
 #!/usr/bin/python3
-""" Script that runs Flask """
-from flask import Flask, make_response, jsonify
-# from flask_cors import CORS
-from models import storage
-from api.v1.views import app_views
+"""
+The RESTful api starts here. The api aids data access in the app.
+"""
 from os import getenv
-from flasgger import Swagger
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config["SWAGGER"] = {
-    "swagger_version": "2.0",
-    "title": "Flasgger",
-    "headers": [
-        ("Access-Control-Allow-Origin", "*"),
-        ("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"),
-        ("Access-Control-Allow-Credentials", "true"),
-    ],
-    "specs": [
-        {
-            "version": "1.0",
-            "title": "HBNB API",
-            "endpoint": "v1_views",
-            "description": "HBNB REST API",
-            "route": "/v1/views",
-        }
-    ],
-}
-swagger = Swagger(app)
+CORS(app, origins=["0.0.0.0"])
+host = getenv("HBNB_API_HOST", "0.0.0.0")
+port = getenv("HBNB_API_PORT", "5000")
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """This method will closes storage session"""
+def teardown(exception):
+    """Cleanup operations"""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Returns 404 status"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    HBNB_API_HOST = getenv("HBNB_API_HOST")
-    HBNB_API_PORT = getenv("HBNB_API_PORT")
-
-    host = "0.0.0.0" if not HBNB_API_HOST else HBNB_API_HOST
-    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
-    app.run(host=host, port=port, threaded=True)
+    app.run(host, port, threaded=True, debug=True)
