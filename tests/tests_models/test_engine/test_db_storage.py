fd6c9c0729025ace78b1541e4b8befb7fb96d71d
lilprof01 <adewwumime@gmail.com>
2025-02-17 20:49:26 +0000

    added two new methods

--- tests/tests_models/test_engine/test_db_storage.py
+++ tests/tests_models/test_engine/test_db_storage.py
@@ -173,3 +173,14 @@ class TestDBStorage(unittest.TestCase):
         dbc1.close()
         cursor.close()
         dbc.close()
+
+    def test_get(self):
+        """Test that get properly returns a requested object"""
+        user = User(name="User1")
+        user.save()
+        self.assertEqual(models.storage.get("User", user.id), user)
+
+    @unittest.skipIf(models.storage_t != "db", "not testing db storage")
+    def test_count(self):
+        """Test that count properly counts all objects"""
+        self.assertEqual(len(models.storage.all()), models.storage.count())
